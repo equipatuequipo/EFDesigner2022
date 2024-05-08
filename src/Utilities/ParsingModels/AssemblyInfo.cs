dadaf4708873aa78ba05312ddd8ca8f41f443cd2
Alexis Santana <alexissantana@tequipo.com>
2024-05-08 13:03:43 +0200

    Update version number

--- src/Utilities/ParsingModels/AssemblyInfo.cs
+++ src/Utilities/ParsingModels/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 
 [assembly : Guid("fb2035a3-09f5-43ff-8545-3af8b814b405")]
 
-[assembly : AssemblyVersion("4.3.4.1")]
-[assembly : AssemblyFileVersion("4.3.4.1")]
+[assembly : AssemblyVersion("4.4.1.1")]
+[assembly : AssemblyFileVersion("4.4.1.1")]
